5186518a6510ffa08e19722662bcc64218cde717
Eisgk <game.080.pf@gmail.com>
2019-01-11 16:50:43 +0700

    Problem 3

--- lap3_3.cpp
+++ lap3_3.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
+
 using namespace std ;
 
 int main(){
 
-    cout << "Hello";
+    double x = 1, y = 2;
+    double sum;
+    while(y<=69){
+
+        sum += x/y;
+        y++;
+    }
+    cout << sum;
     return 0;
 }
